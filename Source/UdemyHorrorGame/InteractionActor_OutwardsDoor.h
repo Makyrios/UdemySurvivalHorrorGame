3da4f817193f29d21014c57f10af17834085a173
Makyrios <lavreniuk.mykyta@lll.kpi.ua>
2023-07-27 17:56:42 +0300

    Outwards door

---
+++ Source/UdemyHorrorGame/InteractionActor_OutwardsDoor.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "InteractionActor_Door.h"
+#include "InteractionActor_OutwardsDoor.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UDEMYHORRORGAME_API AInteractionActor_OutwardsDoor : public AInteractionActor_Door
+{
+	GENERATED_BODY()
+	
+};
