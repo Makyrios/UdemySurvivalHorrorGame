f091194f5bae81a452c26ff2d222d81879b35a3f
Makyrios <lavreniuk.mykyta@lll.kpi.ua>
2023-07-31 21:53:30 +0300

    Inventory widget and layout

---
+++ Source/UdemyHorrorGame/InventoryMenuWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "InventoryMenuWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UDEMYHORRORGAME_API UInventoryMenuWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
